20158fdb9bf524a7ed10935d22923252761b9ca0
Obed Ayisi <oaayisi@gmail.com>
2023-02-06 15:25:00 -0500

    This is a new commit

---
+++ trying.py
@@ -0,0 +1,3 @@
+birthdays = {"Prince": ("March", 1990), "Obed": ("November", 1993), "Edward": ("August", 1997)}
+for key, value in birthdays.items():
+	print(f"{key}'s birthday is {value[0]}, {value[1]}")
